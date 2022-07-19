af525651d03c6675644bfd216c7933b9b6577027
Ian Dao <ibdao921@gmail.com>
2022-07-19 14:56:25 -0700

    added react-router

--- src/JoblyApp.js
+++ src/JoblyApp.js
@@ -1,1 +1,26 @@
 import React from "react";
+
+/** Main JoblyApp Component
+ * 
+ *  Props: 
+ *  - none
+ * 
+ *  State:
+ *  - none
+ *
+ *  Houses routes list
+ *
+ *  JoblyApp -> Routes
+ */
+
+function JoblyApp() {
+  return (
+    <div className="JoblyApp">
+      <BrowserRouter>
+        <Routes />
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default JoblyApp;
