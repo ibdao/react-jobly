94ff493b16a802253fd8742343cd31c4773bd22b
Ian Dao <ibdao921@gmail.com>
2022-07-19 15:05:10 -0700

    meh

--- src/JoblyApp.js
+++ src/JoblyApp.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
-import Routes from "./Routes"
+import RoutesList from "./RoutesList"
 
 /** Main JoblyApp Component
  * 
