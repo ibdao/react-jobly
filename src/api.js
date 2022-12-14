a2f45ec33c027e2027c7ee9babe7cbc80134047c
Ian Dao <ibdao921@gmail.com>
2022-12-13 16:50:04 -0800

    deployed on render

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = process.env.REACT_APP_BASE_URL || "http://localhost:3001";
+const BASE_URL = process.env.REACT_APP_BASE_URL || "https://jobly-ian-dao-backend.onrender.com";
 
 /** API Class.
  *
