53a72e4b8841104562e6edf9b200dd58088f36f0
Ian Dao <ibdao921@gmail.com>
2022-12-13 16:55:19 -0800

    trying to deploy on render

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = process.env.REACT_APP_BASE_URL || "https://jobly-ian-dao-backend.onrender.com";
+const BASE_URL = "https://jobly-ian-dao-backend.onrender.com";
 
 /** API Class.
  *
